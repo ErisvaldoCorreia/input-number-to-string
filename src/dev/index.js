1df89004de379ce572d661ca4d4bd8ebe128c510
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-10-07 08:38:46 -0300

    config: adiciona script para start dev

--- src/dev/index.js
+++ src/dev/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <p>Input Number to String</p>
+const App = () => <h1>Input Number to String</h1>
 
 ReactDOM.render(<App />, document.getElementById("root"));
