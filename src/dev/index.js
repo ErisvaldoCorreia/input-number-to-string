0ac44f98d0eae45edf048d82c060df990c90d077
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-10-07 22:19:00 -0300

    refactor: inclui props e adiciona builder env

--- src/dev/index.js
+++ src/dev/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 import { NumberToString } from '../lib';
 
 //const App = () => <h1>Input Number to String</h1>
 const App = () => {
+    const [numero, setNumero] = useState('');
+
     return(
-        <NumberToString />
+        <div>
+            <NumberToString 
+                returnType="monetario"
+                onChange={numero => setNumero(numero)}
+            />
+            <h3>Numero em Extenso: {numero}</h3>
+        </div>
     )
 }
 
--- src/lib/lib.js
+++ src/lib/lib.js
@@ -2,17 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import numero from 'numero-por-extenso';
 
-const lib = ({onChange, ...props}) => {
+const lib = ({returnType, onChange, ...props}) => {
     return(
         <input
             type="number"
             onChange={e => {
-                const numeroPorExtenso = numero.porExtenso(e.target.value)
+                const numeroPorExtenso = numero.porExtenso(e.target.value, numero.estilo[returnType])
                 onChange(numeroPorExtenso, e);
             }}
             {...props}
         />
     )
+}
+
+NumberToString.propTypes = {
+    returnType: PropTypes.oneOf(["normal", "monetario", "porcentagem"])
+}
+
+NumberToString.defaulProps = {
+    returnType: "normal"
 }
 
 export default lib;
